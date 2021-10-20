06a26907a9ad29bd519dcf2016581915b8eede19
Marcin <marcin.mika123@gmail.com>
2021-10-20 13:11:14 +0200

    ShoeDetail Done

---
+++ actions/get-shoe.js
@@ -0,0 +1,13 @@
+const Shoe = require("./models/shoe-model");
+
+app.get("/shoes", (req, res) => {
+  Shoe.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = { getShoe };
