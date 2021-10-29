9f0fa8352aff958583cf944cff1138a6dcb71a96
marcin <marcin.mika123@gmail.com>
2021-10-29 09:02:53 +0200

    Added config

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN_ALLOWED  || "http://localhost:3000",
     credentials: true,
   })
 );
